7a95000bd6de113183e495e6df36d9717ea91c93
vipyne <vipyne@gmail.com>
2016-10-19 17:58:43 -0400

    cleanup

--- hello-core-profile-triangle.cpp
+++ hello-core-profile-triangle.cpp
@@ -1,29 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// vanessa says hello to a triangle
+// running exe produces a window with a triangle in it!
+//
+// compile : $ make tri
+// usage   : $ make run 
+// example : $ make run
+//           ==> a window with a triangle in it opens. srsly. 
+//
+// shoutout to Lynn for helping me, github.com/alinen
+// author: vanessa pyne --- github.com/vipyne
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-//#include <glbinding/gl/gl.h>
 #include <cstdio>
-#include <OpenGL/gl.h>
 #include <iostream>
+#include <OpenGL/gl.h>
 #include <GLUT/glut.h>
 #include "glint.h"
-
-//using namespace gl;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // Vertex Shader
 //
-const char *vertex_shader_source = "\n" \
-"attribute vec3 pos;        \n" \
-"void main ()          \n" \
-"{                          \n" \
-"   gl_Position = gl_ModelViewProjectionMatrix * vec4(pos, 1.0);    \n" \
-"}                    \n" \
+const char *vertex_shader_source =                              "\n" \
+"attribute vec3 pos;                                             \n" \
+"void main ()                                                    \n" \
+"{																												       \n" \
+"   gl_Position = gl_ModelViewProjectionMatrix * vec4(pos, 1.0); \n" \
+"}                                                               \n" \
 "\n";
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////////////////
 
 // Fragment Shader
 //
@@ -35,43 +41,36 @@ const char *fragment_shader_source = "\n" \
 "\n";
 
 ////////////////////////////////////////////////////////////////////////////////
-GLuint vector_buffer_object_id; // initializing GLuint id
+
+// global variables
 GLuint shaderProgram;
+GLuint vector_buffer_object_id; // initializing GLuint id
 GLuint vertex_array_object_id;
 
-// shadersPointerToVertices = gl.getAttribLocation(webglolProgram, 'pos');
-
-
-void vipInitCanvas()
+void vpInitCanvas()
 {
   glViewport(0, 0, 1000, 1000);
   glClearColor(0.5, 0.2, 0.8, 1.0);
 
   GLfloat vertices[] = { -0.5f, -0.5f, -0.5f,
                           0.5f, -0.5f, -0.5f,
-                          0.0f, 0.5f, -0.5f };   // array of vertices
+                          0.0f,  0.5f, -0.5f };   // array of vertices
 
-  glGenBuffers(1, &vector_buffer_object_id); //passing in ad`dress of id so original value can be changed
-
-  // bind vertex buffer
+	// Buffer Arrays - generate, bind, send data
+  glGenBuffers(1, &vector_buffer_object_id); //passing in address of id so original value can be changed
   glBindBuffer(GL_ARRAY_BUFFER, vector_buffer_object_id);
-
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   // V E R T E X   S H A D E R
   GLuint vertexShader;
-
   vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
   glShaderSource(vertexShader, 1, &vertex_shader_source, NULL);
-
   glCompileShader(vertexShader);
 
   // catch vertex shader compilation error
   GLint success;
   GLchar infoLog[512];
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
   if (!success)
   {
     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
@@ -81,35 +80,27 @@ void vipInitCanvas()
   // F R A G M E N T   S H A D E R
   GLuint fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
   glShaderSource(fragmentShader, 1, &fragment_shader_source, NULL);
-
   glCompileShader(fragmentShader);
 
   // catch fragment shader compilation error
-  GLint f_success;
-  //CLchar infoLog[512];
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &f_success);
-
-  if (!f_success)
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
   {
     glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
     std::cout << "oops. - fragment shader did not compile\n" << infoLog << std::endl;
   }
 
   // S H A D E R   P R O G R A M
-  shaderProgram = glCreateProgram();
-
+  // declared in global scope
+	shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
 
   // catch program linking error
-  GLint p_success;
-  //CLchar infoLog[512];
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &p_success);
-
-  if (!f_success)
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
   {
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
     std::cout << "oops. - shader program did not link\n" << infoLog << std::endl;
@@ -117,25 +108,16 @@ void vipInitCanvas()
 
   glUseProgram(shaderProgram);
 
-  // // like freeing memory?
-  // glDeleteShader(vertexShader);
-  // glDeleteShader(fragmentShader);
-
-
-  // // again that location = 0, in webGL this be `gl.getAttribLocation`
-  // glEnableVertexAttribArray(pos_attr_location); // 0
-
+  // like freeing memory?
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   glGenVertexArraysAPPLE(1, &vertex_array_object_id);
-
   glBindVertexArrayAPPLE(vertex_array_object_id);
 }
 
-
-
-void vipDraw ()
+void vpDraw ()
 {
-  std::cout << "vipDraw" << std::endl;
   glClear(GL_COLOR_BUFFER_BIT);
 
   glMatrixMode(GL_PROJECTION);
@@ -146,15 +128,14 @@ void vipDraw ()
   glLoadIdentity(); // doing matrix math
   glUseProgram(shaderProgram);
 
-
   GLuint pos_attr_location = glGetAttribLocation(shaderProgram, "pos");
 
-  // first arg refers to (location = 0)
-  // values per vertex
-  // not sure when we would ever not use GL_FLOAT here
-  // normalize data to between 0-1 - false because our point data already is normalized
-  // stride - space between consecutive vertex attribute sets
-  // offset of where to start reading from array buffer - needs to be of GLvoid* type
+  // 1 location of attrib we are sending instructions for
+  // 2 values per vertex
+  // 3 not sure when we would ever not use GL_FLOAT here
+  // 4 normalize data to between 0-1 - false because our point data already is normalized
+  // 5 stride - space between consecutive vertex attribute sets
+  // 6 offset of where to start reading from array buffer - needs to be of GLvoid* type
   glVertexAttribPointer(pos_attr_location, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
   glEnableVertexAttribArray(pos_attr_location);
 
@@ -164,21 +145,18 @@ void vipDraw ()
   glFlush();
 }
 
-int main (int argc, char** argv)
+int main (int argc, char *argv[])
 {
-  std::cout << "hi from new version of openGL\n" << std::endl;
+  std::cout << "^^^^ starting program..." << std::endl;
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA);
   glutInitWindowSize(1000, 1000);
   glutInitWindowPosition(50, 50);
-  glutCreateWindow("hi");
-  glutDisplayFunc(vipDraw);
-  vipInitCanvas();
+  glutCreateWindow("oh hi, triangle");
+  glutDisplayFunc(vpDraw);
+  vpInitCanvas();
   glutMainLoop();
-
-  // glbinding::Binding::initialize();
-
-
+	
 	return 0;
 }
